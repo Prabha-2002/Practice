e081e9e87efc549cb80d4e29a79f07b5ab729722
Prabha-2002 <163090094+Prabha-2002@users.noreply.github.com>
2024-03-12 09:51:03 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ class Sample {
   public static void main(String args[]) {
     System.out.println("Hello Everyone");
         System.out.println("Hello Everyone");
+        System.out.println("Hello Everyone welcome");
 
   }
 }
